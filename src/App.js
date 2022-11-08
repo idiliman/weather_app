f743ab7e37139c5dbef2d84ce37199ba1f366076
idil <idil.iman1999@gmail.com>
2022-11-08 16:38:56 +0800

    add mui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import DeviceThermostatIcon from "@mui/icons-material/DeviceThermostat";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Stack from "@mui/material/Stack";
+import Fade from "@mui/material/Fade";
 
 export default function App() {
   const [weatherData, setWeatherData] = useState([]);
@@ -19,26 +24,43 @@ export default function App() {
   };
 
   return (
-    <div className="flex flex-col mx-60">
+    <div className="w-screen h-screen " style={{backgroundImage:`url(https://www.transparenttextures.com/patterns/cubes.png)`}}>
+      <header className="flex flex-col mx-96">
       <input
-        className="w-auto p-1.5 outline-none rounded border border-slate-200 mt-5"
+        className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 w-auto p-1.5 outline-none rounded border border-slate-200 mt-5"
         placeholder="Enter City..."
         onChange={(e) => setCity(e.target.value)}
         value={city}
         onKeyPress={getWeather}
       />
-
+      {/* Weather data UI */}
       {typeof weatherData.main === "undefined" ? (
         <div className="text-center mt-5 ">
-          <p className="">Welcome</p>
+          <p className="text-xl">Enter city to get the weather 🤖</p>
         </div>
       ) : (
-        <div className="text-center space-y-36 text-5xl">
+        <div className="text-center space-y-36 text-5xl mt-24">
           <p>{weatherData.name}</p>
-          <p>{Math.round(weatherData.main.temp)}</p>
+          <p>
+            {Math.round(weatherData.main.temp)}
+            °C
+            <DeviceThermostatIcon />
+          </p>
           <p>{weatherData.weather[0].main}</p>
         </div>
       )}
+      {/* Error UI */}
+      <div className="mt-6 item-center justify-center text-red-500">
+          {weatherData.cod === "404" && (
+            <Stack sx={{ width: "100%" }} spacing={2} className="">
+              <Alert severity="error" className="item-center justify-center">
+                <AlertTitle>Error</AlertTitle>
+                <strong>City not found</strong>
+              </Alert>
+            </Stack>
+          )}
+      </div>
+      </header>
     </div>
   );
 }
