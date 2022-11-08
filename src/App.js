38711ccdb0468664411f44994fa2f2a0e1a796e9
idil <idil.iman1999@gmail.com>
2022-11-08 12:46:22 +0800

    initial with tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+    <h1 className='text-red-500'>Hello World</h1>
+    </header>
+  </div>
+  )
+}
