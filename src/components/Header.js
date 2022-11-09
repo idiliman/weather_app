1ce7decda42d0b038d69b517cab66fe766cd07ff
idil <idil.iman1999@gmail.com>
2022-11-09 12:22:04 +0800

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Header() {
+  return <div className="bg-[#f43f5e]">Header</div>;
+}
---
+++ src/components/Navi.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Navi() {
+  return (
+    <div className='bg-[#ec4899]'>Navi</div>
+  )
+}
